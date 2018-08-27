38ec2edb6c605a6f213ee88ab03e38772c0108b7
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-08-27 13:30:22 +0200

    Add apointment module

--- src/app/views/apointment/apointment.module.ts
+++ src/app/views/apointment/apointment.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApointmentRoutingModule } from './apointment-routing.module';
 import { ApointmentsComponent } from './apointments/apointments.component';
+import { PageHeaderModule, PrimeNg } from '../../shared';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   imports: [
-    CommonModule,ApointmentRoutingModule
+    CommonModule,ApointmentRoutingModule,PageHeaderModule,FormsModule,Ng2SearchPipeModule, ...PrimeNg, NgxPaginationModule
   ],
-  declarations: [ApointmentsComponent]
+  declarations: [ApointmentsComponent],
+  exports:[...PrimeNg]
 })
 export class ApointmentModule { }
