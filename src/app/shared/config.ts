6165370147b639f7512a86ee200da802c34ca11a
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-11-14 19:54:15 +0200

    config

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,11 +1,10 @@
-export const IS_LOCAL = false;
+export const IS_LOCAL = true;
 let api = 'http://dokio.ndu-systems.net/api';
 let web = 'http://dokio.ndu-systems.net';
 if (IS_LOCAL) {
-    api = 'http://localhost:8080/dok-com/api';
+    api = 'httpm://localhost:8080/dok-com/api';
     web = 'http://localhost:4200';
 }
 
 export const   API_URL = api;
 export const  WEB_HOST = web;
-
