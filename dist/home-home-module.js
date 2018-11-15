37576dc1fb9728c6a1fcafb346acb07a61effc82
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-11-15 08:54:52 +0200

    database for priscription

--- dist/home-home-module.js
+++ dist/home-home-module.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<router-outlet></router-outlet>"
+module.exports = "<section id=\"bg\">   \r\n    <app-nav-bar></app-nav-bar>\r\n    <main role=\"main\" class=\"pt-5\">\r\n        <router-outlet></router-outlet>\r\n    </main>\r\n</section>"
 
 /***/ }),
 
@@ -18,7 +18,7 @@ module.exports = "<router-outlet></router-outlet>"
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = ""
+module.exports = "section {\n  font-family: \"Abel\", \"sans-serif\";\n  background-color: #4B4E5A;\n  background-size: auto, cover;\n  background-attachment: fixed, fixed;\n  background-position: center, center;\n  width: 100%;\n  height: 100%;\n  position: absolute;\n  z-index: -1; }\n"
 
 /***/ }),
 
@@ -121,12 +121,14 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
 /* harmony import */ var _home_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./home.component */ "./src/app/home/home.component.ts");
+/* harmony import */ var _nav_bar__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./nav-bar */ "./src/app/home/nav-bar/index.ts");
 var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
 
 
 
@@ -140,7 +142,7 @@ var routes = [
         ]
     },
 ];
-var declarations = [_home_component__WEBPACK_IMPORTED_MODULE_2__["HomeComponent"]];
+var declarations = [_home_component__WEBPACK_IMPORTED_MODULE_2__["HomeComponent"], _nav_bar__WEBPACK_IMPORTED_MODULE_3__["NavBarComponent"]];
 var HomeRoutes = /** @class */ (function () {
     function HomeRoutes() {
     }
@@ -151,6 +153,86 @@ var HomeRoutes = /** @class */ (function () {
         })
     ], HomeRoutes);
     return HomeRoutes;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/home/nav-bar/index.ts":
+/*!***************************************!*\
+  !*** ./src/app/home/nav-bar/index.ts ***!
+  \***************************************/
+/*! exports provided: NavBarComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _nav_bar_component__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./nav-bar.component */ "./src/app/home/nav-bar/nav-bar.component.ts");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "NavBarComponent", function() { return _nav_bar_component__WEBPACK_IMPORTED_MODULE_0__["NavBarComponent"]; });
+
+
+
+
+/***/ }),
+
+/***/ "./src/app/home/nav-bar/nav-bar.component.html":
+/*!*****************************************************!*\
+  !*** ./src/app/home/nav-bar/nav-bar.component.html ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<header class=\"head\">\r\n    <nav class=\"navbar navbar-expand-md  fixed-top\">\r\n        <h3 class=\"ml-5\">\r\n            <a class=\"navbar-brand\" href=\"#\">DokComm</a>\r\n        </h3>\r\n        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarCollapse\"\r\n            aria-controls=\"navbarCollapse\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n            <span class=\"navbar-toggler-icon\"></span>\r\n        </button>\r\n        <div class=\"collapse navbar-collapse\" id=\"navbarCollapse\">\r\n            <ul class=\"navbar-nav ml-auto \">\r\n                <li class=\"nav-item active mr-5\">\r\n                    <a class=\"nav-link\" href=\"#\">Home <span class=\"sr-only\">(current)</span></a>\r\n                </li>\r\n                <li class=\"nav-item  ml-5 mr-5\">\r\n                    <a class=\"nav-link\" href=\"#\">How It Works</a>\r\n                </li>  \r\n                <li class=\"nav-item  ml-5 mr-5\">\r\n                    <a class=\"nav-link\" href=\"#\">Support</a>\r\n                </li>                        \r\n            </ul>\r\n            <ul class=\"navbar-nav mt-2 ml-5 mr-5 mt-md-0\">\r\n                <li class=\"nav-item mr-5\">\r\n                    <a class=\"nav-link\" routerLink=\"/login\">Login</a>\r\n                </li>\r\n                <li class=\"nav-item mr-5\">\r\n                    <a class=\"nav-link btn signup\" href=\"#\">Sign-Up</a>\r\n                </li>\r\n            </ul>\r\n        </div>\r\n    </nav>\r\n</header>"
+
+/***/ }),
+
+/***/ "./src/app/home/nav-bar/nav-bar.component.scss":
+/*!*****************************************************!*\
+  !*** ./src/app/home/nav-bar/nav-bar.component.scss ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "header a {\n  color: white;\n  text-decoration: none;\n  padding: 15px;\n  display: block;\n  text-transform: capitalize;\n  border-bottom: 3px solid transparent; }\n\nli.nav-item a.signup {\n  background: #3498DB;\n  border-radius: 100px;\n  width: 6rem;\n  height: 3.5rem; }\n\nheader nav {\n  background-color: #4B4E5A; }\n\n#navbarCollapse a:hover {\n  border-bottom: 3px solid #3498DB; }\n\n#navbarCollapse li.active {\n  border-bottom: 3px solid #3498DB; }\n"
+
+/***/ }),
+
+/***/ "./src/app/home/nav-bar/nav-bar.component.ts":
+/*!***************************************************!*\
+  !*** ./src/app/home/nav-bar/nav-bar.component.ts ***!
+  \***************************************************/
+/*! exports provided: NavBarComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "NavBarComponent", function() { return NavBarComponent; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+var __decorate = (undefined && undefined.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (undefined && undefined.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var NavBarComponent = /** @class */ (function () {
+    function NavBarComponent() {
+    }
+    NavBarComponent.prototype.ngOnInit = function () {
+    };
+    NavBarComponent = __decorate([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["Component"])({
+            selector: 'app-nav-bar',
+            template: __webpack_require__(/*! ./nav-bar.component.html */ "./src/app/home/nav-bar/nav-bar.component.html"),
+            styles: [__webpack_require__(/*! ./nav-bar.component.scss */ "./src/app/home/nav-bar/nav-bar.component.scss")]
+        }),
+        __metadata("design:paramtypes", [])
+    ], NavBarComponent);
+    return NavBarComponent;
 }());
 
 
