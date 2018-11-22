d447243c706c39c127af1f101715abf251a44ea5
freedomkhanyile <freedom.khanyile1@gmail.com>
2018-11-22 14:49:53 +0200

    merge

--- src/app/models/patient/IPatient.ts
+++ src/app/models/patient/IPatient.ts
@@ -17,14 +17,3 @@ export interface IPatient {
     StatusId: number;
 }
 
-export interface IAddPatient {
-    FirstName: string;
-    Surname: string;
-    Email: string;
-    Cellphone: string;
-    AddressLine1: string;
-    AddressLine2: string;
-    AddressLine3: string;
-    City: string;
-    PostCode: string;  
-}
