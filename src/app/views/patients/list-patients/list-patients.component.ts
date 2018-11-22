29316f23b84412f025d2eaa0f6d451ec09ab0922
freedomkhanyile <freedom.khanyile1@gmail.com>
2018-11-22 14:48:46 +0200

    polishing off the add patient modal

--- src/app/views/patients/list-patients/list-patients.component.ts
+++ src/app/views/patients/list-patients/list-patients.component.ts
@@ -3,7 +3,6 @@ import { SelectService } from '../../../shared';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { routerTransition } from '../../../router.animations';
-import { NgbModal,ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { IAddPatient } from '../../../models';
 
 @Component({
@@ -22,7 +21,7 @@ export class ListPatientsComponent implements OnInit {
   constructor(
     private selectService : SelectService ,
     private route : Router,
-    private modalService: NgbModal
+    
   ) { }
 showAdd(){
     this.showAddModal = true;
@@ -30,7 +29,6 @@ showAdd(){
 onCloseAdd(b: boolean): void {
     this.showAddModal = b;
     this.patients$ = this.selectService.select("patient WHERE  StatusId = 1 ORDER BY CreateDate DESC ");
-
 }
   ngOnInit() {
     this.patients$ = this.selectService.select("patient WHERE  StatusId = 1 ORDER BY CreateDate DESC ");
@@ -39,16 +37,7 @@ onCloseAdd(b: boolean): void {
     this.route.navigate(['/patients/view', patient.PatientId]);
   }
 
-  //modal
-  closeResult: string;
+  
 
-  private getDismissReason(reason: any): string {
-      if (reason === ModalDismissReasons.ESC) {
-          return 'by pressing ESC';
-      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-          return 'by clicking on a backdrop';
-      } else {
-          return `with: ${reason}`;
-      }
-  }
+   
 }
